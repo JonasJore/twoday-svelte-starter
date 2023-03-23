ac3374d42343127236b5ee27ae4f03f6c0eee5d9
JonasJore <jonas.jore@gmail.com>
2023-03-23 22:26:46 +0100

    something :coffee:

--- src/App.svelte
+++ src/App.svelte
@@ -15,7 +15,9 @@
 
   getBreweries().then((data) => {
       // Kanskje litt spaghetti, hehe.
-      let ModifisertLufttrykk = data.map(({Lufttrykk, Timestamp, ...rest}) => ({Lufttrykk: Math.floor(Math.random() * 1501), Timestamp: Timestamp.split("T")[0], ...rest}));
+      let ModifisertLufttrykk = data.map(({Lufttrykk, Timestamp, ...rest}) => (
+        {Lufttrykk: Math.floor(Math.random() * 1501), Timestamp: Timestamp.split("T")[0], ...rest}
+      ));
       breweries = ModifisertLufttrykk
     })
   
